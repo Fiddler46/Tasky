3051b4ffcc606a5e3cff237c923a285cce420f09
Fiddler46 <adhyaksh99@gmail.com>
2024-04-09 10:52:26 +0530

    Main project push

---
+++ src/test/java/com/fiddler46/tasky/TaskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiddler46.tasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
