ca63ea564e8c547d0a6f5fde2c6dc597599d17c1
Fiddler46 <adhyaksh99@gmail.com>
2024-04-11 21:43:18 +0530

    Creation of service layer for general CRUD

---
+++ src/main/java/com/fiddler46/tasky/service/TaskService.java
@@ -0,0 +1,17 @@
+package com.fiddler46.tasky.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.fiddler46.tasky.model.Task;
+import com.fiddler46.tasky.repositories.TaskRepository;
+
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+}
