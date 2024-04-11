da66fb48df31bd6ea113adcf01cd7d94761201cb
Fiddler46 <adhyaksh99@gmail.com>
2024-04-11 16:08:23 +0530

    Set main with restcontroller annotation

--- src/main/java/com/fiddler46/tasky/TaskyApplication.java
+++ src/main/java/com/fiddler46/tasky/TaskyApplication.java
@@ -2,12 +2,16 @@ package com.fiddler46.tasky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TaskyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskyApplication.class, args);
 	}
+
+	
 
 }
