38aa9141c4ecd447e6468e29f0d852c776695240
Fiddler46 <adhyaksh99@gmail.com>
2024-04-11 18:59:44 +0530

    added getters, setters and constructors

--- src/main/java/com/fiddler46/tasky/model/Task.java
+++ src/main/java/com/fiddler46/tasky/model/Task.java
@@ -1,8 +1,83 @@
 package com.fiddler46.tasky.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "Tasks")
 public class Task {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private boolean status;
+    private LocalDateTime dueDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    
+    public Task() {
+        // Default constructor
+    }
 
+    public Task(String title, String description, boolean status, LocalDateTime dueDate) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
+    // Constructors, Getters, and Setters
+    
 }
