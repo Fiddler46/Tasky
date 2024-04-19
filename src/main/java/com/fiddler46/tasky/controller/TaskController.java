12a5402dad4bc3fbea5d16892246877307dd039c
Fiddler46 <adhyaksh99@gmail.com>
2024-04-19 16:04:10 +0530

    getting task by id request added

--- src/main/java/com/fiddler46/tasky/controller/TaskController.java
+++ src/main/java/com/fiddler46/tasky/controller/TaskController.java
@@ -3,9 +3,13 @@ package com.fiddler46.tasky.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.fiddler46.tasky.model.Task;
 import com.fiddler46.tasky.service.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -17,5 +21,13 @@ public class TaskController {
     public List<Task> getAllTasks() {
         return taskService.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable String id) {
+        return taskService.findTaskById(id)
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
 
 }
