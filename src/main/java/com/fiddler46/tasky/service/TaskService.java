9db1c7b3a8a48fee4163f3c8c131fd6dc3688063
Fiddler46 <adhyaksh99@gmail.com>
2024-04-19 16:00:25 +0530

    expanded service layer with CRUD tasks

--- src/main/java/com/fiddler46/tasky/service/TaskService.java
+++ src/main/java/com/fiddler46/tasky/service/TaskService.java
@@ -1,6 +1,9 @@
 package com.fiddler46.tasky.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.fiddler46.tasky.model.Task;
@@ -15,4 +18,28 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
+    public Optional<Task> findTaskById(String id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(String id) {
+        taskRepository.deleteById(id);
+    }
+
+    public ResponseEntity<Task> updateTask(String taskId, Task updatedTask) {
+        return taskRepository.findById(taskId)
+            .map(task -> {
+                task.setTitle(updatedTask.getTitle());
+                task.setDescription(updatedTask.getDescription());
+                task.setStatus(updatedTask.isStatus());
+                task.setDueDate(updatedTask.getDueDate());
+                Task updatedTaskEntity = taskRepository.save(task);
+                return ResponseEntity.ok(updatedTaskEntity);
+            })
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
